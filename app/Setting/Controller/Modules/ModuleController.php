4ef6190d8d54d124ef7fb1ea1ff6728d3d01aba4
geek_inside <38232156+bizvip@users.noreply.github.com>
2024-06-02 20:10:11 +0700

    init

--- app/Setting/Controller/Modules/ModuleController.php
+++ app/Setting/Controller/Modules/ModuleController.php
@@ -1,14 +1,6 @@
 <?php
 
 declare(strict_types=1);
-/**
- * This file is part of MineAdmin.
- *
- * @link     https://www.mineadmin.com
- * @document https://doc.mineadmin.com
- * @contact  root@imoi.cn
- * @license  https://github.com/mineadmin/MineAdmin/blob/master/LICENSE
- */
 
 namespace App\Setting\Controller\Modules;
 
--- app/System/Mapper/SystemPostMapper.php
+++ app/System/Mapper/SystemPostMapper.php
@@ -1,14 +1,6 @@
 <?php
 
 declare(strict_types=1);
-/**
- * This file is part of MineAdmin.
- *
- * @link     https://www.mineadmin.com
- * @document https://doc.mineadmin.com
- * @contact  root@imoi.cn
- * @license  https://github.com/mineadmin/MineAdmin/blob/master/LICENSE
- */
 
 namespace App\System\Mapper;
 
@@ -23,7 +15,7 @@ class SystemPostMapper extends AbstractMapper
      */
     public $model;
 
-    public function assignModel()
+    public function assignModel(): void
     {
         $this->model = SystemPost::class;
     }
@@ -44,11 +36,10 @@ class SystemPostMapper extends AbstractMapper
         }
         if (isset($params['created_at']) && filled($params['created_at']) && is_array($params['created_at']) && count($params['created_at']) == 2) {
             $query->whereBetween(
-                'created_at',
-                [
-                    $params['created_at'][0] . ' 00:00:00',
-                    $params['created_at'][1] . ' 23:59:59',
-                ],
+                'created_at', [
+                $params['created_at'][0] . ' 00:00:00',
+                $params['created_at'][1] . ' 23:59:59',
+            ],
             );
         }
 
